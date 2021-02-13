22069152c2890abbde876c14a1ba058dab032f5f
teitei-tk <teitei.tk@gmail.com>
2021-02-13 19:14:14 +0900

    read server env

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	echo "github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"github.com/teitei-tk/go-next.js-playground/backend"
 	"github.com/teitei-tk/go-next.js-playground/backend/handlers"
 )
 
 func main() {
+	s, err := backend.NewServerEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	ip := "0.0.0.0"
-	port := 5000
-	addr := fmt.Sprintf("%s:%d", ip, port)
+	addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
 	handlers.StartServer(e, addr)
 }
